bb3cd4de70c0d94fe27973aa7360fed2cca50cfe
Naoto Takezawa <naoto9march@gmail.com>
2022-11-06 21:45:50 +0900

    ドキュメンテーションコメントを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+//! 「Go言語でつくるインタプリッタ」に登場する言語「Monkey」のRust実装におけるlibクレート
+
+/// 字句解析器のトークンを扱うモジュール
 pub mod token;
 
 use token::Token;
 
+/// binクレートから呼び出される関数
 pub fn run() {
     println!("Hello, world!");
     println!("Int : {}", Token::Int(112));
